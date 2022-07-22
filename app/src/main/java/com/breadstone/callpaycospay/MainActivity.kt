e34cd295c93a68babe1d7c58f80ed5b7c4df6a88
breadstone <breadstone84@gmail.com>
2022-07-23 01:49:19 +0900

    Initial commit

---
+++ app/src/main/java/com/breadstone/callpaycospay/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.breadstone.callpaycospay
+
+import android.content.ComponentName
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val uri = "payco://open/samsungoffline?servicePath=위젯"
+        Intent( Intent.ACTION_MAIN, Uri.parse(uri)).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            component = ComponentName("com.nhnent.payapp", "com.nhnent.payapp.menu.payment.v2.offline.samsung.SamsungPaymentActivity")
+        }.also {
+            startActivity(it)
+        }
+        finishAndRemoveTask()
+    }
+}
